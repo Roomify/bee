711ac90a144fa6cb29485e6b8709d8ce9d5e268f
Nicolò Caruso <nicolo@istos.it>
2020-01-16 17:15:12 +0100

    Allow users to select which node to book for a BEE enabled webform

--- modules/bee_webform/src/Element/WebformBeeReservationForm.php
+++ modules/bee_webform/src/Element/WebformBeeReservationForm.php
@@ -37,22 +37,6 @@ class WebformBeeReservationForm extends WebformCompositeBase {
       }
 
       if ($content_type_options) {
-        if (count($content_type_options) > 1) {
-          $elements['content_type'] = [
-            '#type' => 'radios',
-            '#title' => t('Content type'),
-            '#required' => TRUE,
-            '#options' => $content_type_options,
-          ];
-        }
-        else {
-          $elements['content_type'] = [
-            '#type' => 'hidden',
-            '#title' => t('Content type'),
-            '#value' => key($content_type_options),
-          ];
-        }
-
         $elements['start_date'] = [
           '#type' => ($bookable_type == 'daily') ? 'date' : 'datetime',
           '#title' => t('Start date'),
@@ -68,6 +52,44 @@ class WebformBeeReservationForm extends WebformCompositeBase {
             '#title' => t('Capacity'),
           ];
         }
+
+        $node_options = [];
+
+        $node_storage = \Drupal::entityTypeManager()->getStorage('node');
+
+        $query = \Drupal::entityQuery('node')
+          ->condition('type', array_keys($content_type_options), 'IN');
+
+        $nids = $query->execute();
+        foreach ($node_storage->loadMultiple($nids) as $node) {
+          $node_options[$node->id()] = $node->label();
+        }
+
+        $states = [
+          [':input[name="' . $element['#webform_key'] . '[start_date][date]"]' => ['value' => '']],
+          'or',
+          [':input[name="' . $element['#webform_key'] . '[end_date][date]"]' => ['value' => '']],
+        ];
+
+        if ($element['#collect_capacity']) {
+          $states[] = 'or';
+          $states[] = [':input[name="' . $element['#webform_key'] . '[capacity]"]' => ['value' => '']];
+        }
+        if ($bookable_type == 'hourly') {
+          $states[] = 'or';
+          $states[] = [':input[name="' . $element['#webform_key'] . '[start_date][time]"]' => ['value' => '']];
+          $states[] = 'or';
+          $states[] = [':input[name="' . $element['#webform_key'] . '[end_date][time]"]' => ['value' => '']];
+        }
+
+        $elements['node'] = [
+          '#type' => 'select',
+          '#title' => t('Node'),
+          '#options' => $node_options,
+          '#states' => [
+            'disabled' => $states,
+          ],
+        ];
 
         $elements['repeat'] = [
           '#type' => 'checkbox',
@@ -114,12 +136,13 @@ class WebformBeeReservationForm extends WebformCompositeBase {
   public static function validateWebformComposite(&$element, FormStateInterface $form_state, &$complete_form) {
     $value = NestedArray::getValue($form_state->getValues(), $element['#parents']);
 
-    if ($value['content_type'] && $value['start_date'] && $value['end_date'] && $value['capacity']) {
-      $available_units = 0;
-      $units = bee_webform_get_available_units($value);
-      $available_units += count($units);
+    $capacity = (isset($value['capacity'])) ? $value['capacity'] : 1;
 
-      if ($available_units < $value['capacity']) {
+    if ($value['node'] && $value['start_date'] && $value['end_date']) {
+      $units = bee_webform_get_available_units($value);
+      $available_units = count($units);
+
+      if ($available_units < $capacity) {
         $form_state->setError($element, t('Unfortunately, not enough units of this type are available'));
       }
     }
