9fe060acf6e42690ab050ee5e0c8b6814321163c
Nicolò Caruso <nicolo@istos.it>
2020-01-17 18:20:21 +0100

    Show only available nodes for the selected dates on a BEE enabled webform

--- modules/bee_webform/src/Element/WebformBeeReservationForm.php
+++ modules/bee_webform/src/Element/WebformBeeReservationForm.php
@@ -40,56 +40,31 @@ class WebformBeeReservationForm extends WebformCompositeBase {
         $elements['start_date'] = [
           '#type' => ($bookable_type == 'daily') ? 'date' : 'datetime',
           '#title' => t('Start date'),
+          '#ajax' => [
+            'callback' => [get_called_class(), 'ajaxCallback'],
+            'wrapper' => 'bee-node-wrapper',
+          ],
         ];
         $elements['end_date'] = [
           '#type' => ($bookable_type == 'daily') ? 'date' : 'datetime',
           '#title' => t('End date'),
+          '#ajax' => [
+            'callback' => [get_called_class(), 'ajaxCallback'],
+            'wrapper' => 'bee-node-wrapper',
+          ],
         ];
 
         if ($element['#collect_capacity']) {
           $elements['capacity'] = [
             '#type' => 'number',
             '#title' => t('Capacity'),
+            '#ajax' => [
+              'callback' => [get_called_class(), 'ajaxCallback'],
+              'wrapper' => 'bee-node-wrapper',
+              'event' => 'change',
+            ],
           ];
         }
-
-        $node_options = [];
-
-        $node_storage = \Drupal::entityTypeManager()->getStorage('node');
-
-        $query = \Drupal::entityQuery('node')
-          ->condition('type', array_keys($content_type_options), 'IN');
-
-        $nids = $query->execute();
-        foreach ($node_storage->loadMultiple($nids) as $node) {
-          $node_options[$node->id()] = $node->label();
-        }
-
-        $states = [
-          [':input[name="' . $element['#webform_key'] . '[start_date][date]"]' => ['value' => '']],
-          'or',
-          [':input[name="' . $element['#webform_key'] . '[end_date][date]"]' => ['value' => '']],
-        ];
-
-        if ($element['#collect_capacity']) {
-          $states[] = 'or';
-          $states[] = [':input[name="' . $element['#webform_key'] . '[capacity]"]' => ['value' => '']];
-        }
-        if ($bookable_type == 'hourly') {
-          $states[] = 'or';
-          $states[] = [':input[name="' . $element['#webform_key'] . '[start_date][time]"]' => ['value' => '']];
-          $states[] = 'or';
-          $states[] = [':input[name="' . $element['#webform_key'] . '[end_date][time]"]' => ['value' => '']];
-        }
-
-        $elements['node'] = [
-          '#type' => 'select',
-          '#title' => t('Node'),
-          '#options' => $node_options,
-          '#states' => [
-            'disabled' => $states,
-          ],
-        ];
 
         $elements['repeat'] = [
           '#type' => 'checkbox',
@@ -123,6 +98,16 @@ class WebformBeeReservationForm extends WebformCompositeBase {
           '#suffix' => '</div>',
         ];
 
+        $elements['node'] = [
+          '#type' => 'select',
+          '#title' => t('Node'),
+          '#options' => [],
+          '#prefix' => '<div id="bee-node-wrapper">',
+          '#suffix' => '</div>',
+          '#disabled' => TRUE,
+          '#empty_option' => '',
+        ];
+
         $elements['#attached']['library'][] = 'bee/bee_form';
       }
     }
@@ -146,6 +131,81 @@ class WebformBeeReservationForm extends WebformCompositeBase {
         $form_state->setError($element, t('Unfortunately, not enough units of this type are available'));
       }
     }
+  }
+
+  public static function ajaxCallback($form, FormStateInterface $form_state) {
+    $element = $form_state->getTriggeringElement();
+    $key = $element['#parents'][0];
+
+    $values = $form_state->getValue($key);
+
+    if (!empty($values['start_date']['date']) && !empty($values['start_date']['time']) &&
+        !empty($values['end_date']['date']) && !empty($values['end_date']['time']) &&
+        (!isset($values['capacity']) || (isset($values['capacity']) && !empty($values['capacity'])))) {
+      $content_types = array_filter($form['elements'][$key]['#content_types']);
+
+      $capacity = (isset($values['capacity'])) ? $values['capacity'] : 1;
+
+      $node_options = [];
+
+      $node_storage = \Drupal::entityTypeManager()->getStorage('node');
+
+      $query = \Drupal::entityQuery('node')
+        ->condition('type', array_keys($content_types), 'IN');
+
+      $nids = $query->execute();
+      foreach ($node_storage->loadMultiple($nids) as $node) {
+        $bee_settings = \Drupal::config('node.type.' . $node->bundle())->get('bee');
+
+        $drupal_units = [];
+        foreach ($node->get('field_availability_' . $bee_settings['bookable_type']) as $unit) {
+          $drupal_units[$unit->entity->id()] = $unit->entity;
+        }
+
+        if ($bee_settings['bookable_type'] == 'daily') {
+          $start_date = new \DateTime($values['start_date']);
+          $end_date = new \DateTime($values['end_date']);
+          $end_date->sub(new \DateInterval('PT1M'));
+
+          $available_units_ids = bat_event_get_matching_units($start_date, $end_date, ['bee_daily_available'], $bee_settings['type_id'], 'availability_daily', FALSE, $drupal_units);
+        }
+        else {
+          $start_date = new \DateTime($values['start_date']['date'] . ' ' . $values['start_date']['time']);
+          $end_date = new \DateTime($values['start_date']['date'] . ' ' . $values['end_date']['time']);
+          $end_date->sub(new \DateInterval('PT1M'));
+
+          $available_units_ids = bat_event_get_matching_units($start_date, $end_date, ['bee_hourly_available'], $bee_settings['type_id'], 'availability_hourly', FALSE, $drupal_units);
+        }
+
+        if (count($available_units_ids) >= $capacity) {
+          $node_options[$node->id()] = $node->label();
+        }
+      }
+
+      $form['elements'][$key]['#webform_composite_elements']['node']['#options'] = $node_options;
+
+      if (empty($node_options)) {
+        \Drupal::messenger()->addError('There are no units available for the selected date/time(s)');
+
+        $form['elements'][$key]['#webform_composite_elements']['node']['#attributes'] = [
+          'disabled' => TRUE,
+        ];
+      }
+    }
+    else {
+      $form['elements'][$key]['#webform_composite_elements']['node']['#options'] = [];
+
+      $form['elements'][$key]['#webform_composite_elements']['node']['#attributes'] = [
+        'disabled' => TRUE,
+      ];
+    }
+
+    $form['elements'][$key]['#webform_composite_elements']['node']['#prefix'] = '<div id="bee-node-wrapper">';
+    $form['elements'][$key]['#webform_composite_elements']['node']['#suffix'] = '</div>';
+
+    $form['elements'][$key]['#webform_composite_elements']['node']['#name'] = $key . '[node]';
+
+    return $form['elements'][$key]['#webform_composite_elements']['node'];
   }
 
 }
